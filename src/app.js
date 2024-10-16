65ab47865cf47851e4fffb5af42bd2431b9a0f28
Ayush <its.as2002@gmail.com>
2024-10-17 03:10:06 +0530

    show middleware(chain of multiple route handler) use

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
 
-app.use("/user", (req, res) => {
-	res.send("Server is saying hi from /user");
+// Handle mutliple route in a single app.use and if want we can pass all the handlers inside a array like app.use("/route",[rh1,rh2,rh3]) or like app.use("/route",[rh1,rh2],rh3) both are right and give same result are normal way.
+app.use(
+	"/user",
+	(req, res, next) => {
+		const token = "abc";
+		if (token != "abc") res.status(404).send("Unauthorized request");
+		else next();
+	},
+	(req, res) => {
+		res.send("Server is saying hi from /user ");
+	}
+);
+
+// handle multiple route separately with extended route with same prefix
+app.use("/hello", (req, res, next) => {
+	console.log("Authenticated");
+	next();
 });
 
+app.get("/hello/abc", (req, res) => {
+	res.send("Saying hi from /hello/abc"); //authenticated from /hello
+});
+
+app.get("/hello/dbcd", (req, res) => {
+	res.send("Saying hllo from /hello/dbcd"); //authentiated from /hello
+});
+
+//
 app.use("/profile", (req, res) => {
 	res.send("Server is responding from /profile");
 });
