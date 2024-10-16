27a2fe14d9e69594424055f9347cb7b71da7d3bc
Ayush <its.as2002@gmail.com>
2024-10-17 04:27:13 +0530

    show WildCard - Error handling of routes

--- src/app.js
+++ src/app.js
@@ -1,35 +1,34 @@
 const express = require("express");
-const { user, admin } = require("./middlewares/auth");
+const { admin } = require("./middlewares/auth");
 const app = express();
 exports.app = app;
 
-//writing code middleware use for authentication as per industry standards for /user and /admin
-app.use("/user", user);
-// app.use("/admin", admin); commented this because there were two route which was admin/login and 2nd one was admin/getAllData so admin login don't need to be authenticated by /admin as it is a login page and there was only one route which need authentication which was /admin/getAllData so i have passed the middleware before the route handler in the callback chain of /admin/getAllData
+// Error handling
 
-// USER routes
-app.get("/user", (req, res) => {
-	res.send("Server is sending response from /user after being authenticated");
-});
-app.get("/user/details", (req, res) => {
-	res.send("Server is sending user details");
-});
-
-app.get("/user/random", (req, res) => {
-	res.send(
-		"Server is respondin to request of /user/random by authenticating user"
-	);
-});
-
-//ADMIN  routes
+// The error thrown by the /admin/login will be catched by the '/' route and handled
 app.get("/admin/login", (req, res) => {
+	throw new Error("Throwing random error");
 	res.send(
 		"Server is veried the login details and redirected to the after login dashboard" //this handler is just for learning concept doesn't really useful as login call are post call. and this route wasn't authenticated.
 	);
 });
 
+// The error thrown by the /admin/getAllData will be catched inside the '/admin/getAllData' catch block and handled there only
 app.get("/admin/getAllData", admin, (req, res) => {
-	res.send("Server is sending all data from /admin/getAllData");
+	try {
+		throw new Error("Throws error which is catched inside a catch block");
+		res.send("Server is sending all data from /admin/getAllData");
+	} catch (err) {
+		console.error(err.message);
+		res.send(err.message);
+	}
+});
+
+app.use("/", (err, req, res, next) => {
+	if (err) {
+		console.error(err.message);
+		res.status(500).send(err.message);
+	}
 });
 
 app.listen(7777, () => {
