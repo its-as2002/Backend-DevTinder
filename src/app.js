d7c9189ec03f59bad92c4cddf42eb7df00c3116e
Ayush <its.as2002@gmail.com>
2024-10-19 03:51:33 +0530

    add dynamic data through signUp Api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
+const { User } = require("./model/user");
 const app = express();
+
+app.use(express.json()); // this middleware converts all the json object to the js object and passes to the request
+app.post("/signUp", async (req, res) => {
+	try {
+		// throw new Error("Something went wrong");
+		const user = new User(req.body); // instance of User Model created
+		await user.save();
+		res.send("User added successfully");
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
 
 connectDB()
 	.then((msg) => {
