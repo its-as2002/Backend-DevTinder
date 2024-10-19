2ecf777eaf15364ac970c60ef2248c9af344d0a4
Ayush <its.as2002@gmail.com>
2024-10-19 16:05:12 +0530

    perform CRUD operations in MongoDB through REST API

--- src/app.js
+++ src/app.js
@@ -15,6 +15,74 @@ app.post("/signUp", async (req, res) => {
 	}
 });
 
+app.get("/getUserByEmail", async (req, res) => {
+	try {
+		const user = await User.findOne(req.body);
+		if (user === null)
+			return res.status(404).send("User not Found with associated emailId");
+		else res.send(user);
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+app.get("/getUserById", async (req, res) => {
+	try {
+		const user = await User.findById(req.body);
+		res.send(user);
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+app.get("/feed", async (req, res) => {
+	try {
+		const users = await User.find({}); //get all users
+		res.send(users);
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+app.delete("/user", async (req, res) => {
+	const idFilter = req.body;
+	try {
+		await User.findByIdAndDelete(idFilter);
+		res.send("User Deleted Successfully");
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+//findOneAndUpdate
+app.patch("/user", async (req, res) => {
+	const filter = req.body.searchFilter;
+	const updateData = req.body.updateData;
+	const options = {
+		returnDocument: "after",
+	};
+	try {
+		const user = await User.findOneAndUpdate(filter, updateData, options);
+		res.send(user);
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
+//findByIdAndUpdate
+app.patch("/userById", async (req, res) => {
+	const filter = { _id: req.body.userId };
+	const updateData = req.body;
+	const options = {
+		returnDocument: "after",
+	};
+	try {
+		const user = await User.findByIdAndUpdate(filter, updateData, options);
+		res.send(user);
+	} catch (err) {
+		res.status(400).send(err.message);
+	}
+});
+
 connectDB()
 	.then((msg) => {
 		console.log(msg);
