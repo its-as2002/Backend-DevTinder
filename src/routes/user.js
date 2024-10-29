5e398d63db4c2ab71678fffdec7477cdd5963f61
Ayush <its.as2002@gmail.com>
2024-10-29 06:14:20 +0530

    Implement user/connections API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../model/connectionRequest");
 const userRouter = express.Router();
-
+const USER_SAFE_DATA = ["firstName", "lastName", "photoURL"];
 //This api will send the pending connection request to the LoggedInUser
 userRouter.get("/requests/recieved", userAuth, async (req, res) => {
 	try {
@@ -11,10 +11,12 @@ userRouter.get("/requests/recieved", userAuth, async (req, res) => {
 			toUserId: loggedInUser._id,
 			status: "interested",
 			// }).populate("fromUserId", "firstName lastName photoURL");
-		}).populate("fromUserId", ["firstName", "lastName", "photoURL"]);
+		}).populate("fromUserId", USER_SAFE_DATA);
+
+		requests = connectionRequest.map((request) => request?.fromUserId);
 		res.status(200).json({
 			message: `Hey ${loggedInUser.firstName}!,You have got ${connectionRequest.length} pending requests`,
-			data: connectionRequest,
+			requests,
 		});
 	} catch (err) {
 		res.status(400).send("Error : " + err.message);
@@ -26,17 +28,25 @@ userRouter.get("/connections", userAuth, async (req, res) => {
 		const loggedInUser = req.user;
 		const connectionRequest = await ConnectionRequest.find({
 			$or: [
-				({ fromUserId: loggedInUser._id, status: "accepted" },
-				{ toUserId: loggedInUser._id, status: "accepted" }),
+				{ fromUserId: loggedInUser._id, status: "accepted" },
+				{ toUserId: loggedInUser._id, status: "accepted" },
 			],
-		}).populate("fromUserId", ["firstName", "lastName", "photoURL"]);
+		})
+			.populate("fromUserId", USER_SAFE_DATA)
+			.populate("toUserId", USER_SAFE_DATA);
 
+		const connections = connectionRequest.map((request) =>
+			loggedInUser._id.equals(request.fromUserId._id)
+				? request?.toUserId
+				: request?.fromUserId
+		);
 		res.json({
 			message: `Hey ${loggedInUser.firstName}!,You have got ${connectionRequest.length} connections`,
-			data: connectionRequest,
+			connections,
 		});
 	} catch (err) {
 		res.status(400).send("Error : " + err.message);
 	}
 });
+
 module.exports = userRouter;
